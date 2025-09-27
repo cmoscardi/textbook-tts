c77e5f301dbfc6d92690f63c46af660977a73de2
Christian Moscardi <christianmoscardi@gmail.com>
2025-09-27 19:32:44 -0400

    even more env stuff whyy

--- supabase-env/custom-init.sh
+++ supabase-env/custom-init.sh
@@ -5,6 +5,9 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # Load environment variables
 source .env
 
+# extra 10 seconds to make sure we can run db push
+sleep 10
+
 $SCRIPT_DIR/../supabase-db-push.sh
 
 # Execute the init.sql file in the database container
